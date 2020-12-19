91ed35bb56321d5a5d4549180956d4d7555af361
Chris Williams <diodesign@tuta.io>
2020-12-19 09:14:24 -0800

    Use build config from TOML file  * Use mason.toml to configure the build run rather than environment variables

--- build.rs
+++ build.rs
@@ -1,11 +1,26 @@
-/* Assemble low-level assembly code and package up binaries for linking with higher-level code
- *
- * This code uses the --target passed to cargo build to determine the tools it needs.
- * It assumes GNU binutils are present on the host system to assemble code and package binaries.
- * It also uses the following environment variables to find files to process:
+/* Mason build script
  * 
- * MASON_ASM_DIRS = Colon-separated directory pathnames of assembly code to assemble
- * MASON_FILES = Colon-separated pathnames of binary files to package up
+ * This assembles low-level assembly code and package up binaries for linking with higher-level code.
+ * It is a cargo-compatible super-build script. It reads the target architecture from the TARGET environment
+ * variable set by cargo --target, and uses that to determine the tools it needs.
+ * It assumes the necessary GNU binutils are present on the host system to assemble code and package binaries.
+ * 
+ * Mason is controlled by a TOML-compliant manifest configuration file named mason.toml.
+ * It will search up the host file system tree from the current working directory for this file .
+ * If no configuration file is found, Mason will exit with an error. The file format is:
+ * 
+ * defaults.include_files = array of binary file pathnames to link with the high-level code.
+ * defaults.asm_dirs = array of directory pathnames of assembly source code to build and link with the high-level code.
+ * target.<target architecture>.include_files = as for defaults but specific to the given architecture
+ * target.<target architecture>.asm_dirs = as for defaults but specific to the given architecture
+ *
+ * All pathnames are relative to the current working directory. All the entries in the config file are optional.
+ * The arrays also stack, meaning that if you define, eg, default and per-target asm_dirs entries, they will be
+ * combined into one array and processed together. Mason ensures a path is included only once: multiple entries
+ * of the same file path will be treated as one.
+ * 
+ * <target architecture> is specified by TARGET, eg: riscv64gc-unknown-none-elf 
+ * Mason also uses the OUT_DIR environment variable, set by cargo, to write its files for linking.
  * 
  * Reminder: this runs on the host build system using the host's architecture.
  * Thus, a Rust toolchain that can build executables for the host arch must be installed, and
@@ -20,12 +35,39 @@
 
 use std::env;
 use std::fs;
-use std::path::Path;
-use std::process::Command;
+use std::path::{Path, PathBuf};
+use std::process::{Command, exit};
 use std::collections::HashSet;
+use std::collections::BTreeMap;
+
+extern crate toml;
+extern crate serde;
+extern crate serde_derive;
+use serde_derive::Deserialize;
 
 extern crate regex;
 use regex::Regex;
+
+/* configuration file name */
+static CONFIG_FILE: &str = "mason.toml";
+
+/* max attempts to search the host file system for a config file */
+static SEARCH_MAX: usize = 100;
+
+/* define the structure of the configuration file */
+#[derive(Deserialize)]
+struct Config
+{
+    defaults: Option<ConfigEntry>,
+    target: Option<BTreeMap<String, ConfigEntry>>
+}
+
+#[derive(Deserialize, Debug)]
+struct ConfigEntry
+{
+    include_files: Option<Vec<String>>,
+    asm_dirs: Option<Vec<String>>
+}
 
 /* describe a build target from its user-supplied triple */
 struct Target
@@ -40,7 +82,7 @@ struct Target
 impl Target
 {
     /* create a target object from a full build triple string, taking the CPU arch from the first part of the triple  */
-    pub fn new(triple: String) -> Target
+    pub fn new(triple: &String) -> Target
     {
         match triple.split('-').next().expect("Badly formatted target triple").as_ref()
         {
@@ -76,6 +118,7 @@ impl Target
 /* shared context of this build run */
 pub struct Context<'a>
 {
+    /* defined by the host environment */
     output_dir: String,       /* where we're outputting object code on the host */
     objects: HashSet<String>, /* set of objects to link, referenced by their full path */
     as_exec: String,          /* path to target's GNU assembler executable */
@@ -88,7 +131,8 @@ pub struct Context<'a>
 fn main()
 {
     /* determine which CPU and platform we're building for from target triple */
-    let target = Target::new(env::var("TARGET").expect("Missing target triple, use --target with cargo"));
+    let target_string = env::var("TARGET").expect("Missing target triple, use --target with cargo");
+    let target = Target::new(&target_string);
 
     /* create a shared context describing this build */
     let mut context = Context
@@ -102,22 +146,39 @@ fn main()
         target: &target
     };
 
-    /* package up individual binary files */
-    if let Some(files) = env::var_os("MASON_FILES")
+    /* get parsed contents of the config file, or bail out if this cannot be obtained */
+    let config = parse_config_file();
+
+    /* populate tables with paths of files to include and assemble from the config file */
+    let mut include_files = HashSet::new();
+    let mut asm_dirs = HashSet::new();
+
+    /* include the defaults */
+    if let Some(defaults) = config.defaults
     {
-        for f in files.into_string().unwrap().split(":")
+        add_file_paths_from_config(&defaults, &mut include_files, &mut asm_dirs);
+    }
+
+    /* select architecture's settings from the given target */
+    if let Some(targets) = config.target
+    {
+        match targets.get(&target_string)
         {
-            package_binary(&String::from(f), &mut context);
+            Some(arch) => add_file_paths_from_config(&arch, &mut include_files, &mut asm_dirs),
+            None => ()
         }
     }
 
-    /* assemble all asm code in each of these directories */
-    if let Some(asm_dir) = env::var_os("MASON_ASM_DIRS")
+    /* package up individual binary files */
+    for f in include_files
     {
-        for dir in asm_dir.into_string().unwrap().split(":")
-        {
-            assemble_directory(String::from(dir), &mut context);
-        }
+        package_binary(&String::from(f), &mut context);
+    }
+
+    /* assemble all asm code in each of these directories */
+    for dir in asm_dirs
+    {
+        assemble_directory(String::from(dir), &mut context);
     }
 
     /* package up all the generated object files into an archive and link against it */
@@ -222,8 +283,7 @@ fn assemble_directory(slurp_from: String, context: &mut Context)
                 if metadata.is_file() == true
                 {
                     println!(
-                        "cargo:rerun-if-changed={}",
-                        file.path().to_str().unwrap()
+                        "cargo:rerun-if-changed={}", file.path().to_str().unwrap()
                     );
                     assemble(file.path().to_str().unwrap(), context);
                 }
@@ -279,7 +339,7 @@ fn assemble(path: &str, mut context: &mut Context)
 /* Create an archive containing all registered .o files and link with this archive */
 fn link_archive(context: &mut Context)
 {
-    let archive_name = String::from("hv");
+    let archive_name = String::from("mason-bundle");
     let archive_path = format!("{}/lib{}.a", &context.output_dir, &archive_name);
 
     /* create archive from .o files in the output directory */
@@ -304,4 +364,92 @@ fn link_archive(context: &mut Context)
     /* tell the linker where to find our archive */
     println!("cargo:rustc-link-search={}", &context.output_dir);
     println!("cargo:rustc-link-lib=static={}", &archive_name);
+}
+
+/* find, load, and parse a configuration file for this run */
+fn parse_config_file() -> Config
+{
+    let config_location = match search_for_config(CONFIG_FILE)
+    {
+        Some(p) => p,
+        None => fatal_error(format!("Can't find configuration file {:?} in host file system", CONFIG_FILE))
+    };
+
+    let config_contents = match fs::read_to_string(&config_location)
+    {
+        Ok(c) => c,
+        Err(e) => fatal_error(format!("Can't read configuration file {:?} in host file system: {}", config_location, e))
+    };
+
+    match toml::from_str(config_contents.as_str())
+    {
+        Ok(c) => c,
+        Err(e) => fatal_error(format!("Can't parse configuration file {:?}: {}", config_location, e))
+    }
+}
+
+/* starting in the current working directory, check for the presence of the
+   required config file, and if it's not there, check inside the parent.
+   continue up the host file system tree until after hitting the root node.
+   this function gives up after SEARCH_MAX iterations to avoid infinite loops.
+   => leafname = config file leafname to look for
+   <= returns filename of found config file, or None if unsuccessful */
+fn search_for_config(leafname: &str) -> Option<PathBuf>
+{
+    let mut path = match env::current_dir()
+    {
+        Ok(p) => p,
+        Err(e) => fatal_error(format!("Can't get the current working directory ({})", e))
+    };
+
+    for _ in 0..SEARCH_MAX
+    {
+        let mut attempt = path.clone();
+        attempt.push(leafname);
+        if attempt.exists() == true
+        {
+            return Some(attempt);
+        }
+
+        path = match path.parent()
+        {
+            Some(p) => p.to_path_buf(),
+            None => return None /* give up if we can't go any higher in the tree */
+        }
+    }
+
+    None
+}
+
+/* parse a ConfigEntry structure and add any found file paths to the given arrays
+   => entry = ConFigEntry structure to parse
+      include_files = table to which 'include_files' string entries will be added
+      asm_dirs = table to which 'asm_dirs' string entries will be added
+*/
+fn add_file_paths_from_config(entry: &ConfigEntry, include_files: &mut HashSet<String>, asm_dirs: &mut HashSet<String>)
+{
+    match &entry.include_files
+    {
+        Some(files) => for file in files
+        {
+            include_files.insert(file.to_string());
+        },
+        None => ()
+    }
+
+    match &entry.asm_dirs
+    {
+        Some(files) => for file in files
+        {
+            asm_dirs.insert(file.to_string());
+        },
+        None => ()
+    }
+}
+
+/* bail out with an error msg */
+fn fatal_error(msg: String) -> !
+{
+    println!("Mason error: {}", msg);
+    exit(1);
 }
